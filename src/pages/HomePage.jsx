87a777c14f9daa98f254b9b65ef5ce16f4f7c929
Fernando Aguilar <feraguilar6985@gmail.com>
2023-08-21 17:35:58 -0600

    add pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "../components/Navbar"
+
+const HomePage = () => {
+  return (
+    <>
+        <Navbar></Navbar>
+        <h1 className="text-3xl font-bold underline">
+        Home Page
+        </h1>
+    </>
+  )
+}
+
+export default HomePage
