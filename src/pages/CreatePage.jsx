3a4b339b7ae2106a1079de9a93472d1687d38836
ServicioRivera <ale.mayen.100@gmail.com>
2023-08-23 17:48:37 -0600

    medic-api-kodigo

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,17 +1,30 @@
+import { appendErrors, useForm } from "react-hook-form";
 import { Navbar } from "../components/Navbar"
 
 const CreatePage = () => {
 
-   /*  const handleSubmit = (event) =>{
-        event.preventDefault()
+    const {register, handleSubmit, formState: {errors}} = useForm()
 
-        const formData = new FormData(event.target)
-        const name = formData.get('name')
-        const age = formData.get('age')
-        console.log(name);
-        console.log(age)
-    } */
+    const onSubmit = async(data) => {
+        try {
+          
+            const response = await fetch('http://localhost:8000/api/patients', {
+              method: 'POST',
+              headers: {
+                'Content-type': 'application/json'
+              },
+              body: JSON.stringify(data)
+            })
 
+            const patient = await response.json()
+            console.log(patient)
+
+        } catch (error) {
+           console.log("ESTO ES UN ERROR POR SI FALLA La API",error)
+        }
+    }
+    
+    
   return (
     <>
         <Navbar></Navbar>
@@ -19,35 +32,39 @@ const CreatePage = () => {
             <div className="md:flex">
                 <div className='p-8 w-full'>
                     <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold mb-1"> Form to add patient</div>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <input 
                             className='shadow appearance-none border rounded w-full m-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
-                            type="text" 
-                            name="name" 
+                            type="text"
+                            {...register("name", {required: true})}
                             placeholder='Patient Name'
                         />
+                        {errors.name && <span className="text-red-500">Este formulario es requerido!</span>}
 
                         <input 
                             className='shadow appearance-none border rounded w-full m-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
                             type="number" 
-                            name="age" 
+                            {...register("age", {required: true})}
                             placeholder='Patient Age'
                         />
 
                         <input 
                             className='shadow appearance-none border rounded w-full m-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
                             type="number" 
-                            name="num_afi" 
+                            {...register("num_afi", {required: true})}
                             placeholder='Patient affiliation number' 
                         />
-
+                        {errors.name && <span className="text-red-500">Este formulario es requerido!</span>}
                         <textarea className='shadow appearance-none border rounded m-3 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                            name="address" 
+                            {...register("address", {required: true})}
                             id="" 
                             cols="30" 
                             rows="10"
                             placeholder='Patient Address'
-                        ></textarea>
+                        >
+                        
+                        </textarea>
+                        {errors.name && <span className="text-red-500">Este formulario es requerido!</span>}
 
                         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='submit'>Add Patient</button>
                     </form>
